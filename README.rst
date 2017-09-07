f743ace57c2b555337038101a54fcb94cc827e07
SimpleGadget512 <simplegadget512@gmail.com>
2017-09-07 10:59:44 -0500

    update PyPI submission files

---
+++ README.rst
@@ -0,0 +1,23 @@
+Truecolor
+=========
+
+A True color terminal library for Python — similar in function to the
+`termcolor library <https://pypi.python.org/pypi/termcolor/1.1.0>`__. It
+allows one to output color text in supported terminals using escape
+codes.
+
+Available functions
+-------------------
+
+-  **bold** - Return a bolder version of a color tuple.
+-  **dim** - Return a dimmer version of a color tuple.
+-  **hex\_to\_rgb** - Return a tuple of (red, green, blue) for a color
+   given as #rrggbb.
+-  **rgb\_to\_hex** - Return color as #rrggbb for the given color tuple
+   or component values.
+-  **fore\_text** - Return text string with foreground only set.
+-  **color\_text** - Return text string with foreground and background
+   set.
+-  **fore\_print** - Print text string with foreground only set.
+-  **color\_print** - Print text string with foreground and background
+   set.
--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='truecolor',
 
-    version='1.0b1',
+    version='1.0b2',
 
-    description='A True color terminal library for Python',
+    description='A Truecolor terminal library for Python',
     long_description=long_description,
 
     url='https://github.com/simplegadget512/Truecolor',
@@ -38,6 +38,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Terminals :: Terminal Emulators/X Terminals',
     ],
@@ -46,7 +47,7 @@ setup(
 
     py_modules=["truecolor"],
 
-    python_requires='>=3',
+    python_requires='>=2.7',
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
