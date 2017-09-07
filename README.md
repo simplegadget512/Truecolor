ff5f9b7edb254d0598898ff3fa3dc9e935ab70e5
SimpleGadget512 <simplegadget512@gmail.com>
2017-09-07 08:55:54 -0500

    Added files for submitting to PyPI

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Truecolor
 
-A True color terminal library for Python. It's similar in function to the [termcolor library](https://pypi.python.org/pypi/termcolor/1.1.0).
+A True color terminal library for Python &#8212; similar in function to the [termcolor library](https://pypi.python.org/pypi/termcolor/1.1.0). It allows one to output color text in supported terminals using escape codes.
 
-##### Available functions
+## Available functions
 * **hex_to_rgb** - Return a tuple of (red, green, blue) for a color given as #rrggbb.
 * **fore_text** - Return text string with foreground only set.
 * **color_text** - Return text string with foreground and background set.
 * **fore_print** - Print text string with foreground only set.
 * **color_print** - Print text string with foreground and background set.
 
-##### TODO
+## TODO
 * implement tests
 * some useful color definitions
---
+++ setup.py
@@ -0,0 +1,59 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='truecolor',
+
+    version='1.0b1',
+
+    description='A True color terminal library for Python',
+    long_description=long_description,
+
+    url='https://github.com/simplegadget512/Truecolor',
+
+    author='Albert Freeman',
+    author_email='simplegadget512@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Terminals :: Terminal Emulators/X Terminals',
+    ],
+
+    keywords='ansi color console terminal',
+
+    py_modules=["truecolor"],
+
+    python_requires='>=3',
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'truecolor=truecolor:main',
+        ],
+    },
+)
